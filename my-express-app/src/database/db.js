6db60b91339a9cef57a405567680237903affdec
Peter Ng <b11902117@ntu.edu.tw>
2024-04-09 21:50:19 +0800

    add db entry init script

--- my-express-app/src/database/db.js
+++ my-express-app/src/database/db.js
@@ -1,6 +1,7 @@
+const fs = require('fs');
 const dbConfig = require("./db.config.js");
-
 const Sequelize = require('sequelize');
+
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
@@ -20,4 +21,10 @@ db.sequelize = sequelize;
 
 db.comment = require("../entities/comment/comment.model.js")(sequelize, Sequelize);
 db.scene = require("../entities/scene/scene.model.js")(sequelize, Sequelize);
+
+// Initialize the database
+// var sql_string = fs.readFileSync('./src/database/scene.sql', 'utf8');
+// sql_string = sql_string.replace(/\r?\n|\r/g, " ");
+// sequelize.query(sql_string);
+
 module.exports = db;
