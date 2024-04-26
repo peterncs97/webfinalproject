12e78ec499b31c43b62a36471d6d71737877f6cd
Peter Ng <b11902117@ntu.edu.tw>
2024-04-26 22:12:17 +0800

    delete comment module

--- my-express-app/src/database/db.js
+++ my-express-app/src/database/db.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
-
+    
     pool: {
         max: dbConfig.pool.max,
         min: dbConfig.pool.min,
@@ -18,7 +18,6 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-//db.comment = require("../entities/comment/comment.model.js")(sequelize, Sequelize);
 db.scene = require("../entities/scene/scene.model.js")(sequelize, Sequelize);
 
 // creature
