ec453734defc7f9b38f660de5cd7a2167e8ba27f
Peter Ng <b11902117@ntu.edu.tw>
2024-05-07 14:27:36 +0800

    fix user character init foreign key problem

--- my-express-app/src/database/user.model.js
+++ my-express-app/src/database/user.model.js
@@ -19,12 +19,14 @@ module.exports = (sequelize, Sequelize, Character) => {
   User.hasOne(Character, {
     foreignKey: {
       name: 'userId',
-    }
+    },
+    onDelete: 'SET NULL'
   });
   Character.belongsTo(User, {
     foreignKey: {
       name: 'userId',
-    }
+    },
+    onDelete: 'SET NULL'
   });
 
   return User;
